a98880b669ac65f14641b09189c99a146bfd17e1
PhongNgh <duchieutran1302@gmail.com>
2025-03-11 18:12:55 +0700

    update for vercel

--- app.py
+++ app.py
@@ -5,14 +5,15 @@ from flask_caching import Cache
 import cloudinary
 from config import Config
 
+# Khởi tạo ứng dụng Flask
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from routes import *
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
+# Khởi tạo các extension
 jwt = JWTManager(app)
 cache = Cache(app)
 mongo = PyMongo(app)
+
+# Import routes sau khi khởi tạo app (để tránh circular import)
+from routes import *
+
