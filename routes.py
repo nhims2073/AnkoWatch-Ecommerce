799bef094b8ce2d1989e9c1c0e269469274064f1
PhongNgh <duchieutran1302@gmail.com>
2025-03-11 09:42:44 +0700

    update route from app.py to routes.py

---
+++ routes.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# User Routes
+@app.route('/')
+def home():
+    return render_template('base.html')
+
+@app.route('/login')
+def login():
+    return render_template('auth/login.html')
+
+@app.route('/register')
+def register():
+    return render_template('auth/register.html')
+
+@app.route('/product/<int:product_id>')
+def product_detail(product_id):
+    return render_template('product/product-detail.html', product_id=product_id)
+
+@app.route('/info')
+def info():
+    return render_template('policy/info.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart/cart.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('cart/checkout.html')
+
+@app.route('/policy')
+def policy():
+    return render_template('policy/policy.html')
+
+# Admin Routes
+@app.route('/admin/dashboard')
+def dashboard():
+    return render_template('admin/dashboard.html')
+
+@app.route('/admin/orders')
+def orders():
+    return render_template('admin/orders.html')
+
+@app.route('/admin/products')
+def products():
+    return render_template('admin/products.html')
+
+@app.route('/admin/customers')
+def customers():
+    return render_template('admin/customers.html')
+
+@app.route('/admin/reports')
+def reports():
+    return render_template('admin/reports.html')
+
+@app.route('/admin/integrations')
+def integrations():
+    return render_template('admin/integrations.html')
