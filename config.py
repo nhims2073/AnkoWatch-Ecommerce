f0d997a2b9806787e805eff15550ea8156391409
PhongNgh <duchieutran1302@gmail.com>
2025-03-26 11:08:45 +0700

    update #3

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 from datetime import timedelta
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "aSgQwIfN0d")
+    SECRET_KEY = os.getenv("SECRET_KEY")
     MONGO_URI = "mongodb+srv://test:12341234@cluster0.i3vkq.mongodb.net/e-commerce?retryWrites=true&w=majority&appName=Cluster0"
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "hOfrkNASyPMHArsIIMretH27L65PdKS8")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     CACHE_TYPE = "RedisCache"
     CACHE_REDIS_HOST = "localhost"
     CACHE_REDIS_PORT = 6379
@@ -15,6 +15,6 @@ class Config:
     JWT_SESSION_COOKIE = True
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
 
-    CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME", "dsbfsc7ii")
-    CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY", "234463982451924")
-    CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET", "oRvQloFeWg2MelgACzu-ev2Kq2U")
+    CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
+    CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
+    CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
